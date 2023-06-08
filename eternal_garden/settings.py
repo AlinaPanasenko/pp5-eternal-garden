1c3f503d49c018299c3fdcd8c49b3f922424b16e
AlinaPanasenko <maikshev@gmail.com>
2023-06-08 16:56:33 +0000

    Add admin, signals and forms

--- eternal_garden/settings.py
+++ eternal_garden/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['eternal-garden.herokuapp.com', 'localhost']
+# ALLOWED_HOSTS = ['eternal-garden.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
